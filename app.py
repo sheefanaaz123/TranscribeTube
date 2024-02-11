f7d8c93a8acf117f545bbfc248566d0c2070e29a
Sheefa Naaz <sheefanaaz6417@gmail.com>
2024-02-11 23:49:58 +0530

    Added new feature

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
 import streamlit as st
 from dotenv import load_dotenv
-
-load_dotenv()  # load all the environment variables
-import os
 import google.generativeai as genai
-
 from youtube_transcript_api import YouTubeTranscriptApi
+import base64
+
+load_dotenv()  
+import os
 
 genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
 
@@ -33,6 +33,23 @@ def generate_gemini_content(transcript_text, prompt):
     model = genai.GenerativeModel("gemini-pro")
     response = model.generate_content(prompt + transcript_text)
     return response.text
+
+def get_binary_file_downloader_html(bin_file, file_label='File', file_name='file'):
+    """
+    Generates a link to download the given binary file.
+    
+    Parameters:
+        bin_file: bytes - The binary file to download.
+        file_label: str - Label of the download link (default: 'File').
+        file_name: str - Name of the file to download (default: 'file').
+    
+    Returns:
+        str: HTML code for downloading the file.
+    """
+    # Convert the binary file to base64
+    bin_file_base64 = base64.b64encode(bin_file.encode()).decode()
+    href = f'<a href="data:application/octet-stream;base64,{bin_file_base64}" download="{file_name}">{file_label}</a>'
+    return href
 
 st.title("YouTube Transcript to Detailed Notes Converter")
 youtube_link = st.text_input("Enter YouTube Video Link:")
@@ -62,9 +79,12 @@ if st.button("Get Detailed Notes"):
 
         summary = generate_gemini_content(transcript_text, prompt)
         
-        # Split the summary into lines
         summaries = summary.split("\n")
         
         st.markdown("## Detailed Notes:")
         for s in summaries:
             st.write(s)
+
+        st.markdown("## Export the notes:")
+        download_text = "\n".join(summaries)
+        st.markdown(get_binary_file_downloader_html(download_text, file_label="Download Text", file_name="summary.txt"), unsafe_allow_html=True)
